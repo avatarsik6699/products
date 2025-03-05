fa3fd27c3fcaa6e748c344a6368c3c799209c058
v.godlevskiy <avatarsik6699@gmail.com>
2025-03-05 22:44:01 +0300

    setting up eslint, prettier. Add dependencies for typeorm/db. Add scripts for migrations,

--- backend/eslint.config.mjs
+++ backend/eslint.config.mjs
@@ -1,35 +1,52 @@
 // @ts-check
-import eslint from '@eslint/js';
-import eslintPluginPrettierRecommended from 'eslint-plugin-prettier/recommended';
-import globals from 'globals';
-import tseslint from 'typescript-eslint';
+import eslint from "@eslint/js";
+import eslintPluginPrettierRecommended from "eslint-plugin-prettier/recommended";
+import globals from "globals";
+import eslintConfigPrettier from "eslint-config-prettier";
+import tseslint from "typescript-eslint";
 
 export default tseslint.config(
-  {
-    ignores: ['eslint.config.mjs'],
-  },
   eslint.configs.recommended,
   ...tseslint.configs.recommendedTypeChecked,
   eslintPluginPrettierRecommended,
+  eslintConfigPrettier,
   {
+    ignores: ["eslint.config.mjs", "node_modules", "dist"],
+
     languageOptions: {
       globals: {
         ...globals.node,
         ...globals.jest,
       },
       ecmaVersion: 5,
-      sourceType: 'module',
+      sourceType: "module",
       parserOptions: {
         projectService: true,
         tsconfigRootDir: import.meta.dirname,
       },
     },
-  },
-  {
+
     rules: {
-      '@typescript-eslint/no-explicit-any': 'off',
-      '@typescript-eslint/no-floating-promises': 'warn',
-      '@typescript-eslint/no-unsafe-argument': 'warn'
+      "@typescript-eslint/no-explicit-any": "off",
+      "@typescript-eslint/no-floating-promises": "warn",
+      "@typescript-eslint/no-unsafe-argument": "warn",
+      "@typescript-eslint/no-namespace": "off",
+
+      "prettier/prettier": [
+        "warn",
+        {
+          endOfLine: "auto",
+          printWidth: 100,
+          tabWidth: 2,
+          useTabs: true,
+          semi: true,
+          singleQuote: false,
+          quoteProps: "consistent",
+          trailingComma: "es5",
+          bracketSpacing: true,
+          arrowParens: "avoid",
+        },
+      ],
     },
-  },
+  }
 );
